8eadd11f3f1ef670051a55a8a68cd82df499787b
momchilosavic <momcilo.savic@hotmail.com>
2019-12-20 15:20:52 +0100

    z11

--- zad11/zad11.py
+++ zad11/zad11.py
@@ -1,8 +1,10 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import random
+
+
+MMAX = 10
+NINT = 3
 
 def dist(A, B):
     sum = 0
@@ -16,61 +18,57 @@ def opt(x):
     A, B, C, D = np.array([[1, 5, 1], [2, 3, 0], [5, 6, 1], [6, 3, 3]])
     return dist(A, S1) + dist(B, S1) + dist(S1, S2) + dist(C, S2) + dist(D, S2)
 
-###############################################################################
 
-#Some variables to calculate the velocity
-W = 0.729
-c1 = 1.494
-c2 = 1.494
-target = 1
+#########################################
 
-n_iterations = 100
-target_error = 0.0001
-n_particles = 50
-D = 6
-particle_position_vector = np.zeros((n_particles, D))
-for i in range(n_particles):
-    particle_position_vector[i] = np.array([np.random.randint(10, size=D)])
+def de(fobj, bounds, mut=0.8, crossp=0.9, popsize=50, its=200):
+    dimensions = len(bounds)                    # broj dimenzija
+    pop = np.random.rand(popsize, dimensions)   # populacija (0 - 1)
+    min_b, max_b = np.asarray(bounds).T         # granice
+    diff = np.fabs(min_b - max_b)               # razlika granica
+    pop_denorm = min_b + pop * diff             # populacija (min - max)
     
-pbest_position = particle_position_vector
-pbest_fitness_value = np.array([float('inf') for _ in range(n_particles)])
-gbest_fitness_value = float('inf')
-gbest_position = np.array([float('inf'), float('inf')])
-
-velocity_vector = ([np.array([0]*D) for _ in range(n_particles)])
-iteration = 0
-while iteration < n_iterations:
-    for i in range(n_particles):
-        fitness_cadidate = opt(particle_position_vector[i])
-        #print(fitness_cadidate, ' ', particle_position_vector[i])
+    fitness = np.asarray([fobj(ind) for ind in pop_denorm]) # vrednost funckije
+    best_idx = np.argmin(fitness)               # indeks minimalne vrednosti funkcije
+    best = pop_denorm[best_idx]                 # minimalna vrednost funkcije
+    for i in range(its):
+        for j in range(popsize):
+            idxs = [idx for idx in range(popsize) if idx != j]
+            a, b, c = pop[np.random.choice(idxs, 3, replace = False)]
+            mutant = np.clip(a + mut * (b - c), 0, 1)
+            cross_points = np.random.rand(dimensions) < crossp
+            if not np.any(cross_points):
+                cross_points[np.random.randint(0, dimensions)] = True
+            trial = np.where(cross_points, mutant, pop[j])
+            trial_denorm = min_b + trial * diff
+            f = fobj(trial_denorm)
+            if f < fitness[j]:
+                fitness[j] = f
+                pop[j] = trial
+                if f < fitness[best_idx]:
+                    best_idx = j
+                    best = trial_denorm
+        yield best, fitness[best_idx]
         
-        if(pbest_fitness_value[i] > fitness_cadidate):
-            pbest_fitness_value[i] = fitness_cadidate
-            pbest_position[i] = particle_position_vector[i]
+bnds = [(np.nextafter(0, 1), 10)] * NINT*2
+result = list(de(opt, bnds))
+print(result[-1])
 
-        if(gbest_fitness_value > fitness_cadidate):
-            gbest_fitness_value = fitness_cadidate
-            gbest_position = particle_position_vector[i]
+x,f = zip(*result)
+plt.plot(f)
 
-    if(abs(gbest_fitness_value - target) < target_error):
-        break
-    
-    for i in range(n_particles):
-        new_velocity = (W*velocity_vector[i]) + (c1*random.random()) * (pbest_position[i] - particle_position_vector[i]) + (c2*random.random()) * (gbest_position-particle_position_vector[i])
-        new_position = new_velocity + particle_position_vector[i]
-        particle_position_vector[i] = new_position
+dim = result[-1][0]
+S1 = np.zeros((3))
+S2 = np.zeros((3))
+S1[0] = dim[0]
+S1[1] = dim[1]
+S1[2] = dim[2]
+S2[0] = dim[3]
+S2[1] = dim[4]
+S2[2] = dim[5]
 
-    iteration = iteration + 1
-    
-print("The best position is ", gbest_position)
-print("fmin ", gbest_fitness_value)
-print("in iteration number ", iteration)
-    
 
-#################
 A, B, C, D = np.array([[1, 5, 1], [2, 3, 0], [5, 6, 1], [6, 3, 3]])
-S1 = np.array(gbest_position[:3])
-S2 = np.array(gbest_position[3:])
 
 fig = plt.figure(figsize=((8,6)), dpi=80)
 ax = fig.add_subplot(111, projection='3d')
